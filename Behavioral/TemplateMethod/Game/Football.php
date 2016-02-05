7bef067323d76a650e84e991e7447f919dc40f96
Oleg Chepkin <olegre@gmail.com>
2016-02-05 10:06:19 +0300

    Template Method

---
+++ Behavioral/TemplateMethod/Game/Football.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: olegchepkin
+ * Date: 05/02/16
+ * Time: 10:01
+ */
+
+namespace Behavioral\TemplateMethod\Game;
+
+
+use Behavioral\TemplateMethod\Game;
+
+class Football extends Game
+{
+
+    protected function init()
+    {
+        // TODO: Implement init() method.
+    }
+
+    protected function start()
+    {
+        // TODO: Implement start() method.
+    }
+
+    protected function end()
+    {
+        // TODO: Implement end() method.
+    }
+}
