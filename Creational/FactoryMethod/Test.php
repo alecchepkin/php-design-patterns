97971811b0256c3b01549be603ce87b51e60de2f
oc <oc@mobioninc.com>
2018-04-19 08:13:06 +0300

    phpunit.xml fix

--- Creational/FactoryMethod/Test.php
+++ Creational/FactoryMethod/Test.php
@@ -12,11 +12,11 @@ namespace Creational\FactoryMethod;
 class Test extends \PHPUnit_Framework_TestCase {
 
     public function testFactoryMethod(){
-    $factory = (new Factory\Plane())->instance();
-    $this->assertInstanceOf('Creational\FactoryMethod\Product\Plane', $factory);
+    $product = (new Factory\Plane())->instance();
+    $this->assertInstanceOf('Creational\FactoryMethod\Product\Plane', $product);
 
-    $factory = (new Factory\Car())->instance();
-    $this->assertInstanceOf('Creational\FactoryMethod\Product\Car', $factory);
+    $product = (new Factory\Car())->instance();
+    $this->assertInstanceOf('Creational\FactoryMethod\Product\Car', $product);
 
     }
 }
